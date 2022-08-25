c2c061256dc7417875095410774a60b1c573f5c5
JDGM0331 <jesus.guevara0@misena.edu.co>
2022-08-25 06:31:41 -0500

    API Users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::apiResource('projects', ProjectController::class)->middleware('auth:api');
